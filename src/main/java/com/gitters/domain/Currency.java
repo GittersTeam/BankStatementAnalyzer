f77a523ebefc7864923dec0fb8b047b6fa3a2b72
Lara-Shaheen <larasha2001@gmail.com>
2021-03-21 23:44:13 +0200

    create Balance classes

--- src/main/java/com/gitters/domain/Currency.java
+++ src/main/java/com/gitters/domain/Currency.java
@@ -1,5 +1,13 @@
 package com.gitters.domain;
 
 public enum Currency {
-    
+
+        USD("$"), NIS("₪"), EURO("€"), GBP("£"),JOD("JD"); 
+        
+        private String value;
+        private Currency(String value){
+        this.value=value;
+        }
+        
+   
 }
--- src/main/java/com/gitters/domain/balance/Balance.java
+++ src/main/java/com/gitters/domain/balance/Balance.java
@@ -1,5 +1,8 @@
 package com.gitters.domain.balance;
 
+import com.gitters.domain.Currency;
+
 public class Balance {
-    
+    double amount;
+    Currency currency;
 }
