db682f09b90b36ce41b8216db93a8da7fd4777cb
Khader Ballout <khaderj@hotmail.com>
2021-03-21 23:05:24 +0200

    BankStatment part one

--- src/main/java/com/gitters/domain/bankStatements/BankStatement.java
+++ src/main/java/com/gitters/domain/bankStatements/BankStatement.java
@@ -1,5 +1,14 @@
 package com.gitters.domain.bankStatements;
 
+import java.util.*;
+
+import com.gitters.domain.transactions.Transaction;
+
 public class BankStatement {
-    
+
+    private String statmentId;
+    private List<Transaction> transaction = new ArrayList<Transaction>();
+
+    public BankStatement() {
+    }
 }
