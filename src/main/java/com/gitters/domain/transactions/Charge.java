933bd52c685a67232320be3dbe3838202b0f6e69
Ramiz Rizqallah <ramiz_rizqallah@hotmail.com>
2021-03-25 00:53:35 +0200

    Added Transactions

--- src/main/java/com/gitters/domain/transactions/Charge.java
+++ src/main/java/com/gitters/domain/transactions/Charge.java
@@ -1,5 +1,44 @@
 package com.gitters.domain.transactions;
 
+import com.gitters.domain.Currency;
+import com.gitters.domain.parsers.AmountParser;
+
 public class Charge {
-    
+	private String amount;
+	private Currency currency;
+
+	public Charge() {
+
+	}
+
+	public Charge(String amount, Currency currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
+
+	public Charge(String amount) {
+		this.amount = amount;
+	}
+
+	public String getAmount() {
+		return amount;
+	}
+
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+
+	public Currency getCurrency() {
+		return currency;
+	}
+
+	public void setCurrency(Currency currency) {
+		this.currency = currency;
+	}
+
+	@Override
+	public String toString() {
+		return "Charge [amount=" + amount + ", currency=" + currency + "]";
+	}
+
 }
